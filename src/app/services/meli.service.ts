3b8b3841325efb58888e615beeefb9883c15944f
GermanPonceDev <germanponcedeveloper@gmail.com>
2024-03-20 18:57:37 -0300

    refresh token

--- src/app/services/meli.service.ts
+++ src/app/services/meli.service.ts
@@ -15,7 +15,7 @@ export class MeliService {
 
   getProduct(item: string):Observable<any>{
     const headers = new HttpHeaders({
-      'Authorization' : 'Bearer APP_USR-3842532818957091-032011-04e6f486bc1a926ff5e545c024e5c4ab-258486877'
+      'Authorization' : 'Bearer APP_USR-3842532818957091-032017-0f6acaf15623421a6e482a1aa90bb47d-258486877'
     });
     const url = this.apiUrl2.concat(item)
     return this.http.get<ProductMeli>(url, {headers});
@@ -23,7 +23,7 @@ export class MeliService {
 
   getProducts(): Observable<any>{
     const headers = new HttpHeaders({
-      'Authorization' : 'Bearer APP_USR-3842532818957091-032011-04e6f486bc1a926ff5e545c024e5c4ab-258486877'
+      'Authorization' : 'Bearer APP_USR-3842532818957091-032017-0f6acaf15623421a6e482a1aa90bb47d-258486877'
     });
     return this.http.get<ProductMeli>(this.apiUrl, {headers});
   }
